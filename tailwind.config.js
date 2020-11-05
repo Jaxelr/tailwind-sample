899a24d9416bf275790692e3b2e7e115746699c6
Jaxel Rojas <jaxelr@gmail.com>
2020-11-05 11:29:18 -0400

    Initial version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    textColor: ['hover', 'group-hover'],
+    backgroundColor: ['hover', 'group-hover'],
+  },
+  plugins: [],
+}
